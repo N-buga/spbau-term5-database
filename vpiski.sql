615be8b192ecba0ab20e44c0849e4cf855d58853
nast1415 <nast1415@gmal.com>
2016-11-16 15:36:16 +0300

    vpiski.sql ids names modified, data generation added

--- vpiski.sql
+++ vpiski.sql
@@ -1,42 +1,42 @@
 CREATE TABLE Countries(
-	id_country SERIAL PRIMARY KEY, 
-	name TEXT not NULL, 
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR(255) not NULL, 
 	commission INT
 );
 
 CREATE TABLE Facilities(
-	id_facility SERIAL PRIMARY KEY, 
-	name TEXT not NULL
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR(255) not NULL
 );
 
 CREATE TABLE EntertainmentsTypes(
-	id_type SERIAL PRIMARY KEY, 
-	name TEXT UNIQUE
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR(255) UNIQUE
 );
 
 CREATE TABLE ReviewsCharacteristics(
-	id_characteristic SERIAL PRIMARY KEY, 
-	name TEXT not NULL
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR(255) not NULL
 );
 
 CREATE TABLE People(
-	id_person SERIAL PRIMARY KEY, 
-	name TEXT not NULL, 
-	second_name TEXT not NULL, 
-	e_mail TEXT not NULL UNIQUE, 
-	phone TEXT not NULL UNIQUE, 
-	sex TEXT CHECK (sex in ('F', 'M')), 
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR(255) not NULL, 
+	second_name VARCHAR(255) not NULL, 
+	e_mail VARCHAR(255) not NULL UNIQUE, 
+	phone VARCHAR(255) not NULL UNIQUE, 
+	sex VARCHAR(255) CHECK (sex in ('F', 'M')), 
 	date_of_birth DATE, 
-	photo TEXT UNIQUE
+	photo VARCHAR(255) UNIQUE
 );
 
 CREATE TABLE Accomodations(
-	id_accomodation SERIAL PRIMARY KEY, 
+	id SERIAL PRIMARY KEY, 
 	user_id INT references People, 
 	country_id INT references Countries, 
-	address TEXT UNIQUE, 
-	gps TEXT UNIQUE, 
-	description TEXT, 
+	address VARCHAR(255) UNIQUE, 
+	gps VARCHAR(255) UNIQUE, 
+	description VARCHAR(255), 
 	rooms_amount INT CHECK (rooms_amount >= 1), 
 	beds_amount INT CHECK (beds_amount >= 0), 
 	max_residents INT CHECK (max_residents >= 1), 
@@ -51,53 +51,53 @@ CREATE TABLE WeeklyCost(
 );
 
 CREATE TABLE ReviewsAccomodation(
-	id_review SERIAL PRIMARY KEY, 
+	id SERIAL PRIMARY KEY, 
 	accomodation_id INT references Accomodations, 
-	review TEXT, 
+	review VARCHAR(255), 
 	user_id INT references People, 
 	reviewed_at TIMESTAMP not NULL
 
 );
 
 CREATE TABLE AccCharacteristics(
-	id_acc_characteristic SERIAL PRIMARY KEY, 
+	id SERIAL PRIMARY KEY, 
 	characteristic_id INT references ReviewsCharacteristics, 
 	review_id INT references ReviewsAccomodation, 
 	mark INT not NULL CHECK(mark <= 5 AND mark >= 1), 
-	comment TEXT,
+	comment VARCHAR(255),
 	UNIQUE(characteristic_id, review_id)
 );
 
 CREATE TABLE AccFacilities(
-	id_acc_facility SERIAL PRIMARY KEY, 
+	id SERIAL PRIMARY KEY, 
 	accomodation_id INT references Accomodations, 
 	facility_id INT references Facilities
 );
 
 CREATE TABLE ReviewsResident(
-	id_review SERIAL PRIMARY KEY, 
+	id SERIAL PRIMARY KEY, 
 	author_id INT references People, 
 	renter_id INT references People, 
-	review TEXT, 
+	review VARCHAR(255), 
 	mark INT not NULL CHECK(mark <= 5 AND mark >= 1)
 );
 
 CREATE TABLE Entertainments(
-	id_entertainment SERIAL PRIMARY KEY, 
-	name TEXT, 
-	gps TEXT, 
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR(255), 
+	gps VARCHAR(255), 
 	start DATE, 
 	finish DATE, 
 	type_id INT references EntertainmentsTypes, 
-	description TEXT
+	description VARCHAR(255)
 );
 
 CREATE TABLE Application(
-	id_application SERIAL PRIMARY KEY, 
+	id SERIAL PRIMARY KEY, 
 	user_id INT references People, 
 	accomodation_id INT references Accomodations, 
 	start_date DATE not NULL, 
 	end_date DATE not NULL, 
-	comment TEXT, 
+	comment VARCHAR(255), 
 	is_accepted BOOLEAN not NULL
 );
